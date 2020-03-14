52f6efde64c6a427de04c4e7f31c2de1f6dd0bb1
ayundadella <ayundadella07@gmail.com>
2020-03-14 10:52:23 +0700

    tugas3

---
+++ app/src/main/java/com/example/intentkot/Activity1.kt
@@ -0,0 +1,28 @@
+package com.example.intentkot
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_1.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Activity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_1)
+
+        val context = this
+
+        //passing atau mengirim data
+        val intentObject:Intent=intent
+        val name=intentObject.getStringExtra("name")
+        textView.text = "HAI $name"
+
+        BtnIntentUtama1.setOnClickListener(){
+            val intent = Intent(context,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
