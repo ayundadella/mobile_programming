5f397f838477f4efda3f7cd42381b411ebf8ccd2
ayundadella <ayundadella07@gmail.com>
2020-05-11 14:12:28 +0700

    tugasfloating_buttpn

---
+++ app/src/main/java/com/example/floating_button/MainActivity.kt
@@ -0,0 +1,21 @@
+
+package com.example.floating_button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab_btn.setOnClickListener {
+
+            Toast.makeText(this, "You clicked over fab button",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
